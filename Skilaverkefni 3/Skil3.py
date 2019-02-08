b4da92f718ed4828a42651defb951f3cf2ee2c95
jonasfreyr <jonasfreyrbjarnason@gmail.com>
2019-02-08 14:12:13 +0000

    Add files via upload

--- Skilaverkefni 3/Skil3.py
+++ Skilaverkefni 3/Skil3.py
@@ -1,15 +1,50 @@
+from itertools import *
+
 staf = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-staf_cache = []
 def strengur(n):
-    s = []
-    for a in range(n):
-        s.append(a)
-    print(s)
-    return r(s)
+    listi = []
+    l = list(combinations(staf, n))
+    for a in l:
+        s = ""
+        for x in a:
+            s += x
+        listi.append(s)
 
-def r(l, m=1):
-    p = l[len(l)-m]
+    return listi
 
-    return p
+def bubble(items):
 
-print(strengur(7))
+    while True:
+        c = 0
+        p = 1
+        swap = False
+        for a in range(len(items)-1):
+            if items[c] > items[p]:
+                swap = True
+                items[c], items[p] = items[p], items[c]
+
+
+            c += 1
+            p += 1
+
+        if swap is False:
+            break
+
+
+    return items
+
+p = strengur(3)
+for a in p:
+    print(a)
+input("Enter to continue")
+print("----------------------------")
+p.reverse()
+print(p)
+input("Enter to continue")
+print("----------------------------")
+p = bubble(p)
+print(p)
+input("Enter to continue")
+
+
+
